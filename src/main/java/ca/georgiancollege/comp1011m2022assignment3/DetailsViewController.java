eacb387d74054b85d4ed5fe81c8dfa80ec1db94c
Georgianciolle <200479322@student.georgianc.on.ca>
2022-08-17 13:32:00 -0400

    created detailsview scene

--- src/main/java/ca/georgiancollege/comp1011m2022assignment3/DetailsViewController.java
+++ src/main/java/ca/georgiancollege/comp1011m2022assignment3/DetailsViewController.java
@@ -1,5 +1,49 @@
 package ca.georgiancollege.comp1011m2022assignment3;
 
-public class DetailsViewController {
+import javafx.event.ActionEvent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DetailsViewController implements Initializable {
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Label movieDetails;
+
+    @FXML
+    void backToSearchViewButtonClicked(ActionEvent event)throws IOException {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("search-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Movie Search");
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
+
+
